acec5a75179094c5e150f380f878256832bf6de1
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-01-12 12:04:14 +0100

    Add html template and js stump

---
+++ README.md
@@ -0,0 +1,8 @@
+# pizza 2.0
+
+Rewrite of pizza using vue.js
+
+
+## Resources
+
+[Tutorial: How to set up React, Webpack 3, and Babel, in 2017](https://www.valentinog.com/blog/react-webpack-babel/)
---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+
+var orders = [
+  {
+    name: "Maximilian",
+    description: "Pizza Funghi, groß",
+    price: "5 €",
+    date: moment().format('DD.MM. h:mm')
+  },
+  {
+    name: "Kai",
+    description: "Pizza Vegetaria, groß",
+    price: "6 €",
+    date: moment().format('DD.MM. h:mm')
+  }
+]
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    pizzeria: {name: "La Scala", url: "https://www.pizzerialascaladortmund.de/"},
+    orders: orders
+  }
+})
